6c47db0588f8939675fa819bdbf6709d7e100664
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-23 16:14:44 +0600

    Refactor product data structure and update ProductsCart component for better display

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,19 +1,26 @@
+import ProductsCart from '@/components/ProductsCart';
 import React from 'react';
 
+
 const productsData=async()=>{
- const res=await fetch('http://localhost:5000/products');
+ const res=await fetch('http://localhost:5000/products',{next:{revalidate:10}});
  const data= await res.json();
  return data;
 }
 
-const page = async () => {
+const product = async () => {
     const products=await productsData();
-    console.log(products);
+
     return (
         <div>
             <h1>Products:{products.length}</h1>
+            <div className="grid grid-cols-3 gap-4 container mx-auto my-10">
+                {
+                    products.map(product=> <ProductsCart key={product.id} product={product}/>)
+                }
+            </div>
         </div>
     );
 };
 
-export default page;
+export default product;
--- src/components/productsCart.jsx
+++ src/components/productsCart.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const productsCart = () => {
-    return (
-        <div>
-            
+const ProductsCart = ({product}) => {
+  return (
+    <div className="card w-96 bg-base-100 card-sm shadow-sm">
+      <div className="card-body">
+        <h2 className="card-title">{product.name}</h2>
+        <p className="font-bold text-3xl">${product.price}</p>
+        <p>
+         {product.description}
+        </p>
+        <div className="justify-end card-actions">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default productsCart;
+export default ProductsCart;
